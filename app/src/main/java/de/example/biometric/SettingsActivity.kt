5151f283fd27777957a25ed156cd7a444ec3f0c1
Daigo Matsuoka <marcelo.daigo@gmail.com>
2018-07-26 18:10:45 +0200

    Second screen with settings

---
+++ app/src/main/java/de/example/biometric/SettingsActivity.kt
@@ -0,0 +1,14 @@
+package de.example.biometric
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+
+    }
+}
