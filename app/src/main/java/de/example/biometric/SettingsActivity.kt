2bc5ec0027d01954d1a039512147eb8b634f303a
Daigo Matsuoka <marcelo.daigo@gmail.com>
2018-07-27 09:35:57 +0200

    Save user preference

--- app/src/main/java/de/example/biometric/SettingsActivity.kt
+++ app/src/main/java/de/example/biometric/SettingsActivity.kt
@@ -2,13 +2,29 @@ package de.example.biometric
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
+
+    private val enableFingerprint by lazy { enable_fingerprint }
+
+    private val fingerprintWrapped by lazy { FingerPrintWrapper(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
+        enableFingerprint.setOnCheckedChangeListener { compoundButton, status ->
+            fingerprintWrapped.saveUser("mobile@shore.com") {
+                it?.let {
+                    Toast.makeText(this@SettingsActivity,
+                            it, Toast.LENGTH_LONG)
+                            .show()
+                }
+            }
+        }
 
+        enableFingerprint.isChecked = fingerprintWrapped.getUser() != null
     }
 }
